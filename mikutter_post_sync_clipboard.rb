10018902e5df394c688d3e77637f70814355cf1d
ncaq <ncaq@ncaq.net>
2019-07-25 12:37:11 +0900

    fixed: mikutter 3.9に対応
    worldのoptionsも含めて返さないと異常終了扱いになるらしい.

--- mikutter_post_sync_clipboard.rb
+++ mikutter_post_sync_clipboard.rb
@@ -1,7 +1,7 @@
 Plugin.create(:mikutter_post_sync_clipboard) do
-  filter_gui_postbox_post do |box|
-    text = ::Plugin.create(:gtk).widgetof(box).widget_post.buffer.text
+  filter_gui_postbox_post do |i_postbox, options = {}|
+    text = ::Plugin.create(:gtk).widgetof(i_postbox).widget_post.buffer.text
     ::Gtk::Clipboard.copy(text)
-    [box]
+    [i_postbox, options]
   end
 end
